173b594a824e68091c1d036f126893f950ceff9f
Sherry Xiong <xsherry22@gmail.com>
2020-02-06 08:43:22 -0500

    first commit

---
+++ src/main/java/com/tts/TinyHorse/TinyHorseApplication.java
@@ -0,0 +1,13 @@
+package com.tts.TinyHorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyHorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyHorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/TinyHorse/TinyHorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.TinyHorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyHorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
